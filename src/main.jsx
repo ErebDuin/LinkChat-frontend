d5ead7d25fa659c0f1da26406b5f8a84e9f2ea75
Artem Oganesyan <artem.oganesyan@gmail.com>
2025-05-29 10:19:36 -0700

    ChatWindow

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Message from './components/Message.jsx'
+import ChatWindow from './components/ChatWindow.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ChatWindow />
   </StrictMode>,
 )
